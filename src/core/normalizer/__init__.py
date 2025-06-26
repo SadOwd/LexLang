c38dec90cabcabc21e7554b94e0b34a108cb0b5c
SHAD <shadowdalia@proton.me>
2025-06-26 23:51:04 +0000

    Create __init__.py

---
+++ src/core/normalizer/__init__.py
@@ -0,0 +1,5 @@
+from .tonal_normalizer import TonalNormalizer
+from .orthographic_normalizer import OrthographicNormalizer
+from .dialect_normalizer import DialectNormalizer
+
+__all__ = ["TonalNormalizer", "OrthographicNormalizer", "DialectNormalizer"]
