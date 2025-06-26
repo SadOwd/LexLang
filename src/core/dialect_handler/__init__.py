9b801c52c33140c347f3be56b17e6ef102dc4402
SHAD <shadowdalia@proton.me>
2025-06-26 23:47:43 +0000

    Create __init__.py

---
+++ src/core/dialect_handler/__init__.py
@@ -0,0 +1,5 @@
+from .dialect_detector import DialectDetector
+from .dialect_converter import DialectConverter
+from .dialect_features import DialectFeatures
+
+__all__ = ["DialectDetector", "DialectConverter", "DialectFeatures"]
